8f7181b67d48d6c31674c0cf39b2e9bc9e84127f
Russell Joyce <russell@russellj.co.uk>
2014-11-17 02:48:46 +0000

    Added screen capture test code

--- Quiz Server/Quiz Server/StartupView.swift
+++ Quiz Server/Quiz Server/StartupView.swift
@@ -11,20 +11,25 @@ import Cocoa
 class StartupView: NSViewController {
     
     @IBOutlet weak var screensLabel: NSTextField!
-
+    @IBOutlet weak var captureButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let allScreens = NSScreen.screens()
         
-        if let screens = allScreens {
+        if let screens = allScreens as? [NSScreen] {
             let numScreens = screens.count
             println("Found \(numScreens) screens:")
             for screen in screens {
                 println("  \(screen.frame)")
             }
             
-            screensLabel.stringValue = "\(numScreens) screens available"
+            screensLabel.stringValue = "\(numScreens - 1) screens available"
+            
+            if numScreens > 1 {
+                captureButton.enabled = true
+            }
         }
         else {
             println("Error enumerating screens");
@@ -32,4 +37,44 @@ class StartupView: NSViewController {
         
     }
     
+    @IBAction func captureScreens(sender: AnyObject) {
+        let allScreens = NSScreen.screens()
+        
+        if let screens = allScreens as? [NSScreen] {
+            for (index, screen) in enumerate(screens) {
+                if index > 0 {
+                    let screenRect = screen.frame
+                    let view = ColourView(frame: screenRect, color: NSColor.greenColor())
+                    let label = NSTextField(frame: CGRectMake(20, 20, screenRect.width - 40, screenRect.height - 40))
+                    label.editable = false
+                    label.stringValue = "Screen \(index)"
+                    view.subviews.append(label)
+                    let fullScreenOptions = [NSFullScreenModeAllScreens: 0]
+                    view.enterFullScreenMode(screen, withOptions: fullScreenOptions)
+                }
+            }
+        }
+        else {
+            println("Error capturing screens");
+        }
+    }
+}
+
+class ColourView: NSView {
+    var color = NSColor.blackColor()
+    
+    init(frame frameRect: NSRect, color: NSColor) {
+        super.init(frame: frameRect)
+        self.color = color
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func drawRect(dirtyRect: NSRect) {
+        self.color.setFill()
+        NSRectFill(dirtyRect)
+        super.drawRect(dirtyRect)
+    }
 }
