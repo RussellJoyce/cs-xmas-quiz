2b7ef2eb6b06fcfccd3917c534991693294dc05b
Russell Joyce <russell.joyce@york.ac.uk>
2014-11-25 20:30:34 +0000

    Bit of work on the Buzzer stuff

--- Teensy/Buzzer/Buzzer.ino
+++ Teensy/Buzzer/Buzzer.ino
@@ -13,8 +13,6 @@
 #define BTN6   2
 #define BTN7  18
 #define BTN8  20
-// #define BTN9  22
-// #define BTN10 23
 #define LED1  14
 #define LED2  16
 #define LED3  12
@@ -23,13 +21,18 @@
 #define LED6   0
 #define LED7  19
 #define LED8  21
-#define LEDB  13
+#define LEDB  13 // On-board LED
+#define LEDS  23 // LED string data
 
 int ledCount = 0;
 
-boolean buttons[8];
-boolean leds[8];
+boolean buttons[] = {false, false, false, false, false, false, false, false};
+boolean oldButtons[] = {false, false, false, false, false, false, false, false};
+boolean leds[] = {false, false, false, false, false, false, false, false};
 boolean reset = false;
+boolean changed = true;
+
+int count = 0;
 
 void setup() {
     // Set up button/LED pins
@@ -41,8 +44,6 @@ void setup() {
     pinMode(BTN6, INPUT);
     pinMode(BTN7, INPUT);
     pinMode(BTN8, INPUT);
-    // pinMode(BTN9, INPUT_PULLUP);
-    // pinMode(BTN10, INPUT_PULLUP);
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
     pinMode(LED3, OUTPUT);
@@ -59,6 +60,7 @@ void setup() {
     // Make game controller use manual event sending
     Quiz.useManualSend(true);
 
+    // Send initial game controller buttons as off
     Quiz.button(1, false);
     Quiz.button(2, false);
     Quiz.button(3, false);
@@ -71,6 +73,15 @@ void setup() {
 }
 
 void loop() {
+    oldButtons[0] = buttons[0];
+    oldButtons[1] = buttons[1];
+    oldButtons[2] = buttons[2];
+    oldButtons[3] = buttons[3];
+    oldButtons[4] = buttons[4];
+    oldButtons[5] = buttons[5];
+    oldButtons[6] = buttons[6];
+    oldButtons[7] = buttons[7];
+
     buttons[0] = digitalRead(BTN1);
     buttons[1] = digitalRead(BTN2);
     buttons[2] = digitalRead(BTN3);
@@ -80,15 +91,26 @@ void loop() {
     buttons[6] = digitalRead(BTN7);
     buttons[7] = digitalRead(BTN8);
 
-    Quiz.button(1, buttons[0]);
-    Quiz.button(2, buttons[1]);
-    Quiz.button(3, buttons[2]);
-    Quiz.button(4, buttons[3]);
-    Quiz.button(5, buttons[4]);
-    Quiz.button(6, buttons[5]);
-    Quiz.button(7, buttons[6]);
-    Quiz.button(8, buttons[7]);
-    Quiz.send_now();
+    changed = ((oldButtons[0] != buttons[0]) ||
+               (oldButtons[1] != buttons[1]) ||
+               (oldButtons[2] != buttons[2]) ||
+               (oldButtons[3] != buttons[3]) ||
+               (oldButtons[4] != buttons[4]) ||
+               (oldButtons[5] != buttons[5]) ||
+               (oldButtons[6] != buttons[6]) ||
+               (oldButtons[7] != buttons[7]));
+
+    if (changed) {
+        Quiz.button(1, buttons[0]);
+        Quiz.button(2, buttons[1]);
+        Quiz.button(3, buttons[2]);
+        Quiz.button(4, buttons[3]);
+        Quiz.button(5, buttons[4]);
+        Quiz.button(6, buttons[5]);
+        Quiz.button(7, buttons[6]);
+        Quiz.button(8, buttons[7]);
+        Quiz.send_now(); 
+    }
 
     leds[0] = buttons[0];
     leds[1] = buttons[1];
@@ -121,4 +143,32 @@ void loop() {
     }
 
     delay(2);
+
+
+    // Code to test buzzer LEDs (alternates output)
+
+    // leds[0] = (count == 0);
+    // leds[1] = (count == 1);
+    // leds[2] = (count == 2);
+    // leds[3] = (count == 3);
+    // leds[4] = (count == 4);
+    // leds[5] = (count == 5);
+    // leds[6] = (count == 6);
+    // leds[7] = (count == 7);
+
+    // digitalWrite(LED1, !leds[0]);
+    // digitalWrite(LED2, !leds[1]);
+    // digitalWrite(LED3, !leds[2]);
+    // digitalWrite(LED4, !leds[3]);
+    // digitalWrite(LED5, !leds[4]);
+    // digitalWrite(LED6, !leds[5]);
+    // digitalWrite(LED7, !leds[6]);
+    // digitalWrite(LED8, !leds[7]);
+
+    // count++;
+    // delay(200);
+
+    // if (count == 8)
+    //     count = 0;
+
 }
