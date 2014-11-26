9b304e01618c24b3a561cf7c5e31232850a5e5b1
Russell Joyce <russell.joyce@york.ac.uk>
2014-11-26 17:59:04 +0000

    Work on Teensy Buzzer serial code Now interrupts every 1ms to read serial and set buzzer LEDs

--- Teensy/Buzzer/Buzzer.ino
+++ Teensy/Buzzer/Buzzer.ino
@@ -7,6 +7,7 @@
 */
 
 #include "FastLED.h"
+#include "commands.h"
 
 #define BTN1  11
 #define BTN2  15
@@ -25,17 +26,95 @@
 #define LED7  19
 #define LED8  21
 #define LEDB  13 // On-board LED
-#define LEDS  23 // LED string data
+#define LEDS  23 // LED string data pin
 
-int ledCount = 0;
 
-boolean buttons[] = {false, false, false, false, false, false, false, false};
-boolean oldButtons[] = {false, false, false, false, false, false, false, false};
-boolean leds[] = {false, false, false, false, false, false, false, false};
-boolean reset = false;
-boolean changed = true;
+volatile uint8_t buttons = 0;
+volatile uint8_t oldButtons = 0;
+volatile uint8_t buzzerLeds = 0;
 
-int count = 0;
+const int ledPins[] = {LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8};
+
+IntervalTimer updateTimer;
+volatile uint8_t serialData;
+volatile uint8_t serialData2;
+volatile uint8_t serialDataHigh;
+volatile uint8_t serialDataLow;
+
+
+inline void outputBuzzerLeds() {
+    digitalWrite(LED1, !bitRead(buzzerLeds, 0));
+    digitalWrite(LED2, !bitRead(buzzerLeds, 1));
+    digitalWrite(LED3, !bitRead(buzzerLeds, 2));
+    digitalWrite(LED4, !bitRead(buzzerLeds, 3));
+    digitalWrite(LED5, !bitRead(buzzerLeds, 4));
+    digitalWrite(LED6, !bitRead(buzzerLeds, 5));
+    digitalWrite(LED7, !bitRead(buzzerLeds, 6));
+    digitalWrite(LED8, !bitRead(buzzerLeds, 7));
+}
+
+inline void outputBuzzerButtons() {
+    Quiz.button(1, bitRead(buttons, 0));
+    Quiz.button(2, bitRead(buttons, 1));
+    Quiz.button(3, bitRead(buttons, 2));
+    Quiz.button(4, bitRead(buttons, 3));
+    Quiz.button(5, bitRead(buttons, 4));
+    Quiz.button(6, bitRead(buttons, 5));
+    Quiz.button(7, bitRead(buttons, 6));
+    Quiz.button(8, bitRead(buttons, 7));
+    Quiz.send_now();
+}
+
+inline void setBuzzerLed(int led, boolean value) {
+    if (bitRead(buzzerLeds, led) != value) {
+        bitWrite(buzzerLeds, led, value);
+        digitalWrite(ledPins[led], !value);
+    }
+}
+
+inline void setBuzzerLedOn(int led) {
+    if (!bitRead(buzzerLeds, led)) {
+        bitSet(buzzerLeds, led);
+        digitalWrite(ledPins[led], false);
+    }
+}
+
+inline void setBuzzerLedOff(int led) {
+    if (bitRead(buzzerLeds, led)) {
+        bitClear(buzzerLeds, led);
+        digitalWrite(ledPins[led], true);
+    }
+}
+
+inline void setBuzzerLeds(uint8_t mask) {
+    if (mask != buzzerLeds) {
+        buzzerLeds = mask;
+        outputBuzzerLeds();
+    }
+}
+
+
+void updateTick() {
+    // Handle any data on serial port
+    if (Serial.available()) {
+        serialData = Serial.read();
+        serialDataHigh = serialData & 0xF0;
+        serialDataLow = serialData & 0x07;
+
+        if (serialDataHigh == LED_ON) {
+            setBuzzerLedOn(serialDataLow);
+        }
+        else if (serialDataHigh == LED_OFF) {
+            setBuzzerLedOff(serialDataLow);
+        }
+        else if (serialData == LED_SET) {
+            while (!Serial.available());
+            serialData2 = Serial.read();
+            setBuzzerLeds(serialData2);
+        }
+    }
+}
+
 
 void setup() {
     // Set up button/LED pins
@@ -57,95 +136,93 @@ void setup() {
     pinMode(LED8, OUTPUT);
     pinMode(LEDB, OUTPUT);
 
-    // Set up serial
-    Serial.begin(115200);
+    // Set up serial (Teensy implicity uses full USB bandwidth of 12Mb/s)
+    Serial.begin(0);
 
     // Make game controller use manual event sending
     Quiz.useManualSend(true);
 
     // Send initial game controller buttons as off
-    Quiz.button(1, false);
-    Quiz.button(2, false);
-    Quiz.button(3, false);
-    Quiz.button(4, false);
-    Quiz.button(5, false);
-    Quiz.button(6, false);
-    Quiz.button(7, false);
-    Quiz.button(8, false);
-    Quiz.send_now();
+    outputBuzzerButtons();
+
+    // Set buzzer LEDs to be off
+    outputBuzzerLeds();
+
+    // Start update timer to interrupt every millisecond
+    updateTimer.begin(updateTick, 1000);
 }
 
 void loop() {
-    oldButtons[0] = buttons[0];
-    oldButtons[1] = buttons[1];
-    oldButtons[2] = buttons[2];
-    oldButtons[3] = buttons[3];
-    oldButtons[4] = buttons[4];
-    oldButtons[5] = buttons[5];
-    oldButtons[6] = buttons[6];
-    oldButtons[7] = buttons[7];
+    // oldButtons[0] = buttons[0];
+    // oldButtons[1] = buttons[1];
+    // oldButtons[2] = buttons[2];
+    // oldButtons[3] = buttons[3];
+    // oldButtons[4] = buttons[4];
+    // oldButtons[5] = buttons[5];
+    // oldButtons[6] = buttons[6];
+    // oldButtons[7] = buttons[7];
 
-    buttons[0] = digitalRead(BTN1);
-    buttons[1] = digitalRead(BTN2);
-    buttons[2] = digitalRead(BTN3);
-    buttons[3] = digitalRead(BTN4);
-    buttons[4] = digitalRead(BTN5);
-    buttons[5] = digitalRead(BTN6);
-    buttons[6] = digitalRead(BTN7);
-    buttons[7] = digitalRead(BTN8);
+    // buttons[0] = digitalRead(BTN1);
+    // buttons[1] = digitalRead(BTN2);
+    // buttons[2] = digitalRead(BTN3);
+    // buttons[3] = digitalRead(BTN4);
+    // buttons[4] = digitalRead(BTN5);
+    // buttons[5] = digitalRead(BTN6);
+    // buttons[6] = digitalRead(BTN7);
+    // buttons[7] = digitalRead(BTN8);
 
-    changed = ((oldButtons[0] != buttons[0]) ||
-               (oldButtons[1] != buttons[1]) ||
-               (oldButtons[2] != buttons[2]) ||
-               (oldButtons[3] != buttons[3]) ||
-               (oldButtons[4] != buttons[4]) ||
-               (oldButtons[5] != buttons[5]) ||
-               (oldButtons[6] != buttons[6]) ||
-               (oldButtons[7] != buttons[7]));
+    // changed = ((oldButtons[0] != buttons[0]) ||
+    //            (oldButtons[1] != buttons[1]) ||
+    //            (oldButtons[2] != buttons[2]) ||
+    //            (oldButtons[3] != buttons[3]) ||
+    //            (oldButtons[4] != buttons[4]) ||
+    //            (oldButtons[5] != buttons[5]) ||
+    //            (oldButtons[6] != buttons[6]) ||
+    //            (oldButtons[7] != buttons[7]));
 
-    if (changed) {
-        Quiz.button(1, buttons[0]);
-        Quiz.button(2, buttons[1]);
-        Quiz.button(3, buttons[2]);
-        Quiz.button(4, buttons[3]);
-        Quiz.button(5, buttons[4]);
-        Quiz.button(6, buttons[5]);
-        Quiz.button(7, buttons[6]);
-        Quiz.button(8, buttons[7]);
-        Quiz.send_now(); 
-    }
+    // if (changed) {
+    //     Quiz.button(1, buttons[0]);
+    //     Quiz.button(2, buttons[1]);
+    //     Quiz.button(3, buttons[2]);
+    //     Quiz.button(4, buttons[3]);
+    //     Quiz.button(5, buttons[4]);
+    //     Quiz.button(6, buttons[5]);
+    //     Quiz.button(7, buttons[6]);
+    //     Quiz.button(8, buttons[7]);
+    //     Quiz.send_now(); 
+    // }
 
-    leds[0] = buttons[0];
-    leds[1] = buttons[1];
-    leds[2] = buttons[2];
-    leds[3] = buttons[3];
-    leds[4] = buttons[4];
-    leds[5] = buttons[5];
-    leds[6] = buttons[6];
-    leds[7] = buttons[7];
+    // leds[0] = buttons[0];
+    // leds[1] = buttons[1];
+    // leds[2] = buttons[2];
+    // leds[3] = buttons[3];
+    // leds[4] = buttons[4];
+    // leds[5] = buttons[5];
+    // leds[6] = buttons[6];
+    // leds[7] = buttons[7];
 
-    digitalWrite(LED1, !leds[0]);
-    digitalWrite(LED2, !leds[1]);
-    digitalWrite(LED3, !leds[2]);
-    digitalWrite(LED4, !leds[3]);
-    digitalWrite(LED5, !leds[4]);
-    digitalWrite(LED6, !leds[5]);
-    digitalWrite(LED7, !leds[6]);
-    digitalWrite(LED8, !leds[7]);
+    // digitalWrite(LED1, !leds[0]);
+    // digitalWrite(LED2, !leds[1]);
+    // digitalWrite(LED3, !leds[2]);
+    // digitalWrite(LED4, !leds[3]);
+    // digitalWrite(LED5, !leds[4]);
+    // digitalWrite(LED6, !leds[5]);
+    // digitalWrite(LED7, !leds[6]);
+    // digitalWrite(LED8, !leds[7]);
 
-    //Serial.println("Hello!");
+    // //Serial.println("Hello!");
 
-    // Blink the board LED
-    ledCount ++;
-    if (ledCount == 800) {
-        digitalWrite(LEDB, true);
-    }
-    else if (ledCount == 1000) {
-        digitalWrite(LEDB, false);
-        ledCount = 0;
-    }
+    // // Blink the board LED
+    // ledCount ++;
+    // if (ledCount == 800) {
+    //     digitalWrite(LEDB, true);
+    // }
+    // else if (ledCount == 1000) {
+    //     digitalWrite(LEDB, false);
+    //     ledCount = 0;
+    // }
 
-    delay(2);
+    // delay(2);
 
 
     // Code to test buzzer LEDs (alternates output)
@@ -173,5 +250,5 @@ void loop() {
 
     // if (count == 8)
     //     count = 0;
-
 }
+
