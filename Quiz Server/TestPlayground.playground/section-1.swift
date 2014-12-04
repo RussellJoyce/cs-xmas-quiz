9a038b244be2b2f4bbc12183cde6d9726d77ca4a
Russell Joyce <russell.joyce@york.ac.uk>
2014-12-04 13:42:16 +0000

    Fun with Jose in the playground

--- Quiz Server/TestPlayground.playground/section-1.swift
+++ Quiz Server/TestPlayground.playground/section-1.swift
@@ -2,17 +2,24 @@
 
 import Cocoa
 
-//let allScreens = NSScreen.screens()
-//
-//if let screens = allScreens {
-//    print(screens.count)
-//
-//    for screen in screens {
-//        print(screen.frame)
-//    }
-//}
+protocol TreeLike {}
+enum Tree:TreeLike {
+    case Empty
+    case Leaf(Int)
+    case Node(TreeLike, TreeLike)
+}
 
+let a = Tree.Node(Tree.Node(Tree.Leaf(1), Tree.Leaf(2)), Tree.Leaf(3))
 
-//import DDHidLib
+func sum (tree: Tree) -> Int {
+    switch tree {
+    case .Empty:
+        return 0
+    case let .Leaf(a):
+        return a
+    case let .Node(l, r):
+        return sum(l as Tree) + sum(r as Tree)
+    }
+}
 
-//let joysticks = DDHidJoystick.allJoysticks()
+sum(a)
