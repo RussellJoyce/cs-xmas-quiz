cd4502ad0d287125872d0d7a08c7de687366caf0
Russell Joyce <russell.joyce@york.ac.uk>
2014-12-08 18:05:02 +0000

    Documentation for QuizLeds class!

--- Quiz Server/Quiz Server/QuizLeds.swift
+++ Quiz Server/Quiz Server/QuizLeds.swift
@@ -17,37 +17,63 @@ let LED_ON  = 0xA0 as Byte
 let LED_OFF = 0xB0 as Byte
 let LED_SET = 0xC0 as Byte
 
+/// Controller for the quiz buzzer system LEDs (both buzzer LEDs and LED string)
 class QuizLeds: NSObject {
     let serial: ORSSerialPort
     
+    /// Initialise LEDs connected via serial port
+    ///
+    /// :param: serialPort Serial port of the buzzer system
     init(serialPort: ORSSerialPort) {
         serial = serialPort
     }
     
+    /// Open associated serial port (required once before use)
     func openSerial() {
         serial.open()
     }
     
+    /// Close associated serial port (call when finished using LEDs)
+    ///
+    /// :returns: true if port was closed successfully, false otherwise
     func closeSerial() -> Bool {
         return serial.close()
     }
     
+    /// Turn all buzzer LEDs off
+    ///
+    /// :returns: true if data sent successfully, false otherwise
     func allOff() -> Bool {
         return serial.sendData(NSData(bytes: [LED_SET, 0x00] as [Byte], length: 2));
     }
     
+    /// Turn all buzzer LEDs on
+    ///
+    /// :returns: true if data sent successfully, false otherwise
     func allOn() -> Bool {
         return serial.sendData(NSData(bytes: [LED_SET, 0xFF] as [Byte], length: 2));
     }
     
+    /// Turn a specific buzzer LED off
+    ///
+    /// :param: led The LED to to turn off (0-7)
+    /// :returns: true if data sent successfully, false otherwise
     func ledOff(led: Byte) -> Bool {
         return serial.sendData(NSData(bytes: [LED_OFF + led] as [Byte], length: 1));
     }
     
+    /// Turn a specific buzzer LED on
+    ///
+    /// :param: led The LED to to turn on (0-7)
+    /// :returns: true if data sent successfully, false otherwise
     func ledOn(led: Byte) -> Bool {
         return serial.sendData(NSData(bytes: [LED_ON + led] as [Byte], length: 1));
     }
     
+    /// Set animation on LED string
+    ///
+    /// :param: animation The animation number (0-7)
+    /// :returns: true if data sent successfully, false otherwise
     func setAnimation(animation: Byte) -> Bool {
         return serial.sendData(NSData(bytes: [LEDS_ANIM + animation] as [Byte], length: 1));
     }
