1579c1298b1ce377870e3a890b888b4b40dfbec8
Ian Gray <iangray001@github.com>
2014-12-08 15:56:12 +0000

    Added PointlessGameController and integrated switching

---
+++ Quiz Server/Quiz Server/PointlessGameController.swift
@@ -0,0 +1,18 @@
+//
+//  PointlessGameController.swift
+//  Quiz Server
+//
+//  Created by Ian Gray on 08/12/2014.
+//  Copyright (c) 2014 Russell Joyce. All rights reserved.
+//
+
+import Cocoa
+
+class PointlessGameController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
