d62cd3da0f3b6169ff26d3742be4463ad26d863d
Ian Gray <iangray001@github.com>
2014-12-08 23:06:51 +0000

    Work on PointlessGameController

--- Quiz Server/Quiz Server/PointlessGameController.swift
+++ Quiz Server/Quiz Server/PointlessGameController.swift
@@ -10,9 +10,70 @@ import Cocoa
 
 class PointlessGameController: NSViewController {
 
+	@IBOutlet weak var labelstack: NSStackView!
+	@IBOutlet weak var pv: PointlessView!
+	
+	var labels = [PGLabelView]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+		
+		for i in 1...8 {
+			let v = PGLabelView(teamno: i)
+			labels.append(v)
+			labelstack.addView(v, inGravity: NSStackViewGravity.Center)
+		}
+		
+		pv.layer!.borderWidth = 4
+		pv.layer!.borderColor = NSColor(red: 0, green: 0, blue: 0, alpha: 1).CGColor
     }
-    
+	
 }
+
+
+
+
+class PGLabelView : NSView {
+	let teamno : Int
+	let label = NSTextField()
+	
+	init(teamno : Int) {
+		self.teamno = teamno
+		super.init()
+		self.translatesAutoresizingMaskIntoConstraints = false
+		setMinSize(self, 300, 30)
+		self.wantsLayer = true
+		//self.layer!.backgroundColor = NSColor(red: 1, green: 0, blue: 0, alpha: 1).CGColor
+		
+		label.editable = false
+		label.drawsBackground = false
+		label.bezeled = false
+		
+		label.font = NSFont(name: "Oriya MN Bold", size: 48)
+		
+		label.stringValue = "Team " + String(teamno)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(label)
+		constrainToSizeOfContainer(label, self)
+	}
+	
+	required init?(coder: NSCoder) {
+		self.teamno = 0
+		super.init(coder: coder)
+	}
+	
+	override init(frame frameRect: NSRect) {
+		self.teamno = 0
+		super.init(frame: frameRect)
+	}
+}
+
+
+class PGMainView: NSView {
+	let bgImage = NSImage(named: "purple-texture")
+	override func drawRect(dirtyRect: NSRect) {
+		bgImage?.drawInRect(dirtyRect)
+		self.wantsUpdateLayer
+	}
+}
+
