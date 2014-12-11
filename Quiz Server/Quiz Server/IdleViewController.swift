a0a66bb47523c397c3cd8cb91d3fb7081c958c60
Russell Joyce <russell@russellj.co.uk>
2014-12-11 00:18:00 +0000

    Added snow to Idle view!

--- Quiz Server/Quiz Server/IdleViewController.swift
+++ Quiz Server/Quiz Server/IdleViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import Cocoa
+import SpriteKit
 
 class IdleViewController: NSViewController {
 
+    @IBOutlet weak var skView: SKView!
+    
+    let scene = SKScene()
     var leds: QuizLeds?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set up SpriteKit scene/view
+        skView.allowsTransparency = true
+        scene.size = skView.bounds.size
+        scene.backgroundColor = NSColor.clearColor()
+        skView.presentScene(scene)
+        let node = SKEmitterNode(fileNamed: "Snow")
+        node.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 5)
+        scene.addChild(node)
     }
     
     
