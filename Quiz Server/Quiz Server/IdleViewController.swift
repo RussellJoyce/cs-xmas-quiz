14993d6aaaa61ed2ace4b362a7ecd51b9acf1216
Russell Joyce <russell.joyce@york.ac.uk>
2015-11-12 14:16:58 +0000

    Added SNOWMOJI easter egg to Idle view

--- Quiz Server/Quiz Server/IdleViewController.swift
+++ Quiz Server/Quiz Server/IdleViewController.swift
@@ -16,13 +16,9 @@ class IdleViewController: NSViewController {
     
     let scene = SKScene()
     let snow = SKEmitterNode(fileNamed: "Snow")!
-    //let poo = SKEmitterNode(fileNamed: "Poo")!
-    let santa = SKEmitterNode(fileNamed: "Santa")!
-    let trees = SKEmitterNode(fileNamed: "Tree")!
+    var snowmojis = [SKEmitterNode]()
     var leds: QuizLeds?
-    var snowAmount = CGFloat(40.0)
-    var buzzerStates = [Bool](count: 10, repeatedValue: false)
-    
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,26 +29,28 @@ class IdleViewController: NSViewController {
 		bgImage.position = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
 		bgImage.size = scene.size
         snow.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 16)
-        //poo.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 32)
-        santa.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 32)
-        trees.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 32)
-        //scene.addChild(poo)
+		snow.particleBirthRate = 40
+		
+		for i in 0...9 {
+			let snowmoji = SKEmitterNode(fileNamed: "Snowmoji")!
+			snowmoji.particleTexture = SKTexture(imageNamed: "snowmoji\(i)")
+			snowmoji.position = CGPoint(x: scene.size.width / 2, y: scene.size.height + 32)
+			snowmojis.append(snowmoji)
+		}
+		
 		scene.addChild(bgImage)
-        scene.addChild(santa)
-        scene.addChild(trees)
+		for node in snowmojis {
+			scene.addChild(node)
+		}
         scene.addChild(snow)
 		skView.presentScene(scene)
     }
     
     func reset() {
         leds?.stringAnimation(2)
-        snowAmount = 40
-        snow.particleBirthRate = snowAmount
-        //poo.particleBirthRate = 0
-        santa.particleBirthRate = 0
-        trees.particleBirthRate = 0
-        
-        buzzerStates = [Bool](count: 10, repeatedValue: false)
+		for node in snowmojis {
+			node.particleBirthRate = 0
+		}
 		
 		// Add animated lights
 		let images = [NSImage(named: "lights4")!, NSImage(named: "lights3")!, NSImage(named: "lights2")!, NSImage(named: "lights1")!, NSImage(named: "lights4")!]
@@ -66,28 +64,11 @@ class IdleViewController: NSViewController {
     }
     
     func buzzerPressed(team: Int) {
-        if !buzzerStates[team] {
-            snowAmount += 50
-            snow.particleBirthRate = snowAmount
-            buzzerStates[team] = true
-            
-            if buzzerStates == [true, true, true, true, true, true, true, true, true, true] {
-                //poo.particleBirthRate = 20
-                santa.particleBirthRate = 15
-                trees.particleBirthRate = 15
-            }
-        }
+		snowmojis[team].particleBirthRate = 20
     }
     
     func buzzerReleased(team: Int) {
-        if buzzerStates[team] {
-            snowAmount -= 50
-            snow.particleBirthRate = snowAmount
-            buzzerStates[team] = false
-            //poo.particleBirthRate = 0
-            santa.particleBirthRate = 0
-            trees.particleBirthRate = 0
-        }
+        snowmojis[team].particleBirthRate = 0
     }
     
 }
