49c192eefcf944013139bf0ac1b3df1e8889e1a9
Ian Gray <iangray001@github.com>
2016-11-30 15:40:47 +0000

    Added new node modules and consolidated various branches

--- Quiz Server/Quiz Server/GeographyScene.swift
+++ Quiz Server/Quiz Server/GeographyScene.swift
@@ -15,6 +15,8 @@ class GeographyScene: SKScene {
 	fileprivate var setUp = false
 	var numTeams = 10
 	
+	let text = SKLabelNode(fontNamed: ".AppleSystemUIFontBold")
+	
 	func setUpScene(size: CGSize, leds: QuizLeds?, numTeams: Int) {
 		if setUp {
 			return
@@ -25,7 +27,43 @@ class GeographyScene: SKScene {
 		self.leds = leds
 		self.numTeams = numTeams
 		
-		self.backgroundColor = NSColor.green
+		let bgImage = SKSpriteNode(imageNamed: "purple-texture-blurred")
+		bgImage.zPosition = 0.0
+		bgImage.position = CGPoint(x:self.frame.midX, y:self.frame.midY)
+		bgImage.size = self.size
+		
+		self.addChild(bgImage)
+		
+		let mainImage = SKSpriteNode(imageNamed: "2")
+		mainImage.position = CGPoint(x: 0, y: 50.0)
+		mainImage.size.width = 1300.0
+		mainImage.size.height = 867.0
+		mainImage.zPosition = 1.0
+		bgImage.addChild(mainImage)
+		
+		
+		text.fontSize = 70
+		text.fontColor = NSColor.white
+		text.horizontalAlignmentMode = .left
+		text.verticalAlignmentMode = .baseline
+		text.zPosition = 6.0
+		text.position = CGPoint(x: 50, y: 50)
+		text.text = "Teams Remaining: 1 2 3 4 5 6 7 8"
+		
+		self.addChild(text)
+
+	}
+	
+	func setQuestion(question: Int) {
+		
+	}
+	
+	func go() {
+		
+	}
+	
+	func teamAnswered(team: Int, x: Int, y: Int) {
+		
 	}
 	
 	func reset() {
