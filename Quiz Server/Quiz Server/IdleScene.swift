42ea6a7e543dc174530641a019e9187e8c0a7393
Russell Joyce <russell.joyce@york.ac.uk>
2018-12-06 14:17:02 +0000

    TIdied up idle view, added/fixed various snowmoji 🥳

--- Quiz Server/Quiz Server/IdleScene.swift
+++ Quiz Server/Quiz Server/IdleScene.swift
@@ -14,9 +14,8 @@ class IdleScene: SKScene {
 	var snowmojis = [SKEmitterNode]()
 	var leds: QuizLeds?
 	fileprivate var setUp = false
-	let emoji = ["tree", "santa", "spaceinvader", "robot", "snowman", "present", "floppydisk", "snowflake", "mortarboard", "poop"]
-	let clock = ClockNode()
-	
+	let emoji = ["tree", "santa", "spaceinvader", "robot", "snowman", "present", "floppydisk", "snowflake", "party", "crazy"]
+
 	func setUpScene(size: CGSize, leds: QuizLeds?) {
 		if setUp {
 			return
@@ -77,21 +76,11 @@ class IdleScene: SKScene {
 			snowmojis.append(snowmoji)
 		}
 		
-//		let quizSnow = SKEmitterNode(fileNamed: "Snowmoji")!
-//		quizSnow.particleTexture = SKTexture(imageNamed: "quiz")
-//		quizSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
-//		quizSnow.zPosition = 11
-//		quizSnow.particleRotationSpeed = 1.0
-//		quizSnow.particleBirthRate = 0.04
-		
 		let ianSnow = SKEmitterNode(fileNamed: "Snowmoji")!
 		ianSnow.particleTexture = SKTexture(imageNamed: "ian")
 		ianSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
 		ianSnow.zPosition = 11
 		ianSnow.particleRotationSpeed = 1.0
-		ianSnow.particleScale = 0.1
-		ianSnow.particleScaleSpeed = 0
-		ianSnow.particleScaleRange = 0.05
 		ianSnow.particleBirthRate = 0.04
 		
 		let richardSnow = SKEmitterNode(fileNamed: "Snowmoji")!
@@ -113,14 +102,35 @@ class IdleScene: SKScene {
 		ooSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
 		ooSnow.zPosition = 11
 		ooSnow.particleRotationSpeed = 2.0
-		ooSnow.particleBirthRate = 0.03
+		ooSnow.particleBirthRate = 0.009
 		
 		let nootSnow = SKEmitterNode(fileNamed: "Snowmoji")!
 		nootSnow.particleTexture = SKTexture(imageNamed: "nootnoot")
 		nootSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
 		nootSnow.zPosition = 11
 		nootSnow.particleRotationSpeed = 1.0
-		nootSnow.particleBirthRate = 0.009
+		nootSnow.particleBirthRate = 0.024
+		
+		let poopSnow = SKEmitterNode(fileNamed: "Snowmoji")!
+		poopSnow.particleTexture = SKTexture(imageNamed: "poop")
+		poopSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
+		poopSnow.zPosition = 11
+		poopSnow.particleRotationSpeed = 1.0
+		poopSnow.particleBirthRate = 0.01
+		
+		let coldSnow = SKEmitterNode(fileNamed: "Snowmoji")!
+		coldSnow.particleTexture = SKTexture(imageNamed: "cold")
+		coldSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
+		coldSnow.zPosition = 11
+		coldSnow.particleRotationSpeed = 1.0
+		coldSnow.particleBirthRate = 0.035
+		
+		let drunkSnow = SKEmitterNode(fileNamed: "Snowmoji")!
+		drunkSnow.particleTexture = SKTexture(imageNamed: "drunk")
+		drunkSnow.position = CGPoint(x: self.size.width / 2, y: self.size.height + 80)
+		drunkSnow.zPosition = 11
+		drunkSnow.particleRotationSpeed = 1.0
+		drunkSnow.particleBirthRate = 0.02
 		
 		let text1 = SKLabelNode(fontNamed: "Neutra Display Titling")
 		text1.text = "Computer Science"
@@ -186,12 +196,12 @@ class IdleScene: SKScene {
 //		let lightsAction = SKAction.repeatForever(SKAction.animate(with: lightsTextures, timePerFrame: 1.0))
 //		lights.run(lightsAction)
 		
+		let clock = ClockNode()
 		clock.position = CGPoint(x: 1450, y: 260)
 		clock.zPosition = 50
 		clock.updateTime(animated: false)
-		
-		Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
-			self.clock.updateTime(animated: true)
+		Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (_) in
+			clock.updateTime(animated: true)
 		}
 		
 		for node in snowmojis {
@@ -208,12 +218,14 @@ class IdleScene: SKScene {
 		self.addChild(text)
 		self.addChild(textShadow)
 //		self.addChild(lights)
-//		self.addChild(quizSnow)
 		self.addChild(ianSnow)
 		self.addChild(richardSnow)
 		self.addChild(eggSnow)
 		self.addChild(ooSnow)
 		self.addChild(nootSnow)
+		self.addChild(poopSnow)
+		self.addChild(coldSnow)
+		self.addChild(drunkSnow)
 		self.addChild(clock)
 	}
 	
