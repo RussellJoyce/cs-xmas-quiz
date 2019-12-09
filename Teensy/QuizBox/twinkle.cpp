1f0a71f4bee5f2283c3aeed036831a288fd9a357
Russell Joyce <russell.joyce@york.ac.uk>
2019-12-09 19:17:22 +0000

    Made LED twinkle animation more twinkly

--- Teensy/QuizBox/twinkle.cpp
+++ Teensy/QuizBox/twinkle.cpp
@@ -2,10 +2,10 @@
 
 Twinkle *twinkle = new Twinkle();
 
-#define SPEED            2  // Every speed frames change a target colour
-#define HUE             60  // The hue of the leds
-#define SATURATION     200  // The saturation of the leds
-#define TWINKLE_AMOUNT 255  // The amount to twiddle the value of the leds
+#define SPEED            4  // Every speed frames change a target colour
+#define HUE             85  // The hue of the leds
+#define SATURATION     100  // The saturation of the leds
+#define TWINKLE_AMOUNT  60  // The amount to twiddle the value of the leds
 
 #define FADEFRAMES 4
 
@@ -14,11 +14,11 @@ CHSV current[NUM_LEDS];
 
 void Twinkle::start() {
 	for(int i = 0; i < NUM_LEDS; i++) {
-		leds[i] = CHSV(HUE, SATURATION, 255);
-		target[i] = CHSV(HUE, SATURATION, 255);
-		current[i] = CHSV(HUE, SATURATION, 255);
+		leds[i] = CHSV(HUE, SATURATION, 60);
+		target[i] = CHSV(HUE, SATURATION, 60);
+		current[i] = CHSV(HUE, SATURATION, 60);
 	}
-	//FastLED.show();
+	// FastLED.show();
 }
 
 void Twinkle::tick() {
@@ -30,7 +30,11 @@ void Twinkle::tick() {
 
 	if(framenum > SPEED) {
 		framenum = 0;
-		target[random(NUM_LEDS)] = CHSV(HUE, SATURATION, 255 - random(TWINKLE_AMOUNT));
+		int s = SATURATION + 20 - random(40);
+		int v = random(TWINKLE_AMOUNT) + 25;
+		if (random(15) == 0)
+			v *= 3;
+		target[random(NUM_LEDS)] = CHSV(HUE, s, v);
 	}
 
 	if(fadeframenum > FADEFRAMES) {
@@ -38,7 +42,8 @@ void Twinkle::tick() {
 		for(int i = 0; i < NUM_LEDS; i++) {
 			if(current[i].v < target[i].v) current[i].v++;
 			if(current[i].v > target[i].v) current[i].v--;
-			current[i].s = SATURATION;
+			if(current[i].s < target[i].s) current[i].s++;
+			if(current[i].s > target[i].s) current[i].s--;
 			current[i].h = HUE;
 			leds[i] = current[i];
 		}
